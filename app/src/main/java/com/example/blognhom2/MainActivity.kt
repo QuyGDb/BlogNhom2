debd2c03434597f48080b7e35192972fc28d1770
mitsuha <usoppgod2003@gmail.com>
2024-05-04 19:34:10 +0700

    done navigationbar

--- app/src/main/java/com/example/blognhom2/MainActivity.kt
+++ app/src/main/java/com/example/blognhom2/MainActivity.kt
@@ -5,36 +5,41 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.blognhom2.Fragment.*
 import com.example.blognhom2.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
-    private  lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        transFragment()
         //chuyển qua các fragment
-        binding.bottomBar.setOnItemSelectedListener {
-            when(it){
-                R.id.home -> replaceFragment(HomeFragment())
-                R.id.categories -> replaceFragment(CategoriesFragment())
-                R.id.write -> replaceFragment(WriteFragment())
-                R.id.bookmark -> replaceFragment(BookmarkFragment())
-                R.id.profile -> replaceFragment(ProfileFragment())
 
-                else ->{
+    }
+
+    // hàm chuyển fragment
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun transFragment() {
+        binding.bottomBar.setOnItemSelectedListener {
+            when (it) {
+                0 -> replaceFragment(HomeFragment())
+                1 -> replaceFragment(CategoriesFragment())
+                2 -> replaceFragment(WriteFragment())
+                3 -> replaceFragment(BookmarkFragment())
+                4 -> replaceFragment(ProfileFragment())
+
+                else -> {
 
                 }
 
             }
-            true
         }
-    }
-    // hàm chuyển fragment
-    private  fun  replaceFragment(fragment : Fragment){
-    val fragmentManager = supportFragmentManager
-        val  fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.commit()
     }
 }
