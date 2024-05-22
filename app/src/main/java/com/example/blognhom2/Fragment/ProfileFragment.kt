a8507e522506184e4dd76f5a6845c279de529d92
tanthanh2901 <nguyentanthanh29012003@gmail.com>
2024-05-22 13:40:52 +0700

    test profile fragment

--- app/src/main/java/com/example/blognhom2/Fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/blognhom2/Fragment/ProfileFragment.kt
@@ -18,8 +18,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-
-
 class ProfileFragment : Fragment() {
     private var isLoading = false
     private var visibleThreshold = 5 // Number of items from the bottom of the list at which loading more is triggered
@@ -77,6 +75,67 @@ class ProfileFragment : Fragment() {
                 println(t.message)
             }
         })
+
+        val post1 = PostInfo(123,
+            "TQ",
+            "29/01/2003",
+            "https://raw.githubusercontent.com/jackson22153fake/BlogImgRepository/main/56a4d870-e3f1-4085-b6ef-2aae4d17641b.jpg",
+            "The Dark Knight",
+            "Movie",
+            "Để giới hạn số ký tự được hiển thị trong một TextView trong ứng dụng Android Studio, bạn có thể sử dụng một số phương pháp khác nhau. Dưới đây là một số cách phổ biến:",
+            "ok"
+        )
+        val post2 = PostInfo(456,
+            "TQ",
+            "",
+            "https://raw.githubusercontent.com/jackson22153fake/BlogImgRepository/main/b8f2ad85-2cae-47d5-af6a-1db3432fe5c3.jpg",
+            "Sekiro: Shadows Die Twice - Vẻ đẹp ẩn sau lớp vỏ khó nhằn",
+            "Anime",
+            "testcontent",
+            "ok"
+        )
+        val post3 = PostInfo(789,
+            "TQ",
+            "LocalDate.now()",
+            "https://raw.githubusercontent.com/jackson22153fake/BlogImgRepository/main/849dca8f-4ab9-4eb9-99db-34ad6c74d0c2.png",
+            "Elden Ring - Siêu phẩm hay game rác?",
+            "Music",
+            "testcontent",
+            "Ok"
+        )
+        val post4 = PostInfo(0,
+            "12",
+            "LocalDate.now()",
+            "https://raw.githubusercontent.com/jackson22153fake/BlogImgRepository/main/56a4d870-e3f1-4085-b6ef-2aae4d17641b.jpg",
+            "Vô gian đạo",
+            "Movie",
+            "testcontent",
+            "Ok"
+        )
+        val post5 = PostInfo(1234,
+            "TQ",
+            "LocalDate.now()",
+            "https://raw.githubusercontent.com/jackson22153fake/BlogImgRepository/main/b8f2ad85-2cae-47d5-af6a-1db3432fe5c3.jpg",
+            "Lie of P",
+            "Game",
+            "testcontent",
+            "ok"
+        )
+        val post6 = PostInfo(5678,
+            "1",
+            "LocalDate.now()",
+            "https://raw.githubusercontent.com/jackson22153fake/BlogImgRepository/main/849dca8f-4ab9-4eb9-99db-34ad6c74d0c2.png",
+            "Ngọt",
+            "Music",
+            "testcontent",
+            "ok"
+        )
+        postList.add(post1)
+        postList.add(post2)
+        postList.add(post3)
+        postList.add(post4)
+        postList.add(post5)
+        postList.add(post6)
 
         return postList
     }
