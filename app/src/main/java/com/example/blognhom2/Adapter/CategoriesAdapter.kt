afaba274d462b9952987d22fd298c7a9277da3fd
Trịnh Quý <usoppgod2003@gmail.com>
2024-05-24 17:40:44 +0700

    auto load post for category post

--- app/src/main/java/com/example/blognhom2/Adapter/CategoriesAdapter.kt
+++ app/src/main/java/com/example/blognhom2/Adapter/CategoriesAdapter.kt
@@ -21,7 +21,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 class CategoriesAdapter(var categoriesList : List<Category>, var posts : MutableList<PostInfo>): RecyclerView.Adapter<CategoriesAdapter.CategoriesViewHolder>() {
     private  var postsByCategories = mutableListOf<PostInfo>()
     private var categoriesFragment = CategoriesFragment()
-
+    private var isLoading = false
+    private var visibleThreshold = 5 // Number of items from the bottom of the list at which loading more is triggered
+    private var offset = 0 // The offset for loading more posts
     inner class CategoriesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val categoriesTxt: TextView = itemView.findViewById(R.id.categoriesTxt)
         val categoriesImage: ImageView = itemView.findViewById(R.id.categoriesImage)
@@ -55,8 +57,6 @@ class CategoriesAdapter(var categoriesList : List<Category>, var posts : Mutable
                 holder.childRecyclerView.visibility = if(categoriesList[position].isExpandable) View.VISIBLE else View.GONE
                 FindPostByCategory(holder.categoriesTxt.text.toString(),holder)
 
-
-
             }
 
         }
@@ -68,11 +68,55 @@ class CategoriesAdapter(var categoriesList : List<Category>, var posts : Mutable
                 val adapter = PostAdapter(postInfos)
                 holder.childRecyclerView.adapter = adapter
                 holder.childRecyclerView.layoutManager = LinearLayoutManager(holder.itemView.context, LinearLayoutManager.HORIZONTAL,false)
+                holder.childRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
+
+                        val totalItemCount = (holder.childRecyclerView.layoutManager as LinearLayoutManager).itemCount
+                        val lastVisibleItem = (holder.childRecyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+
+                        if (!isLoading && totalItemCount <= (lastVisibleItem + visibleThreshold)) {
+                            // End has been reached, load more items
+                            loadMoreItems(category, holder.childRecyclerView.adapter as PostAdapter)
+                            isLoading = true
+                        }
+                    }
+                })
             }
         }
 
     }
+    private fun loadMoreItems(category: String, adapter : PostAdapter) {
+        // Increase your offset
+        offset += 1
 
+        // Call your API here
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8081/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(PostApi::class.java)
+        val call = api.getPostsByCategory(category, offset);
+
+        call.enqueue(object : Callback<List<PostInfo>> {
+            override fun onResponse(call: Call<List<PostInfo>>, response: Response<List<PostInfo>>) {
+                println(response)
+                if (response.isSuccessful) {
+                    val posts = response.body()
+                    posts?.let {
+                        postsByCategories.addAll(it)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+                isLoading = false
+            }
+
+            override fun onFailure(call: Call<List<PostInfo>>, t: Throwable) {
+                // Handle the error
+                isLoading = false
+            }
+        })
+    }
     private fun getPostsByCategory(category: String, page: Int, onResult: (List<PostInfo>?) -> Unit){
         val retrofit = Retrofit.Builder()
             .baseUrl("http://10.0.2.2:8081/")
