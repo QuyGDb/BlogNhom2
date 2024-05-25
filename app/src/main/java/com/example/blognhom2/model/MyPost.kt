b8ff3982bd7ad29292bf6163126eab365aca7382
nguyenvanquyen2203 <nguyenvanquyentin9@gmail.com>
2024-05-26 03:29:28 +0700

    Nguyen Van Quyen

---
+++ app/src/main/java/com/example/blognhom2/model/MyPost.kt
@@ -0,0 +1,10 @@
+package com.example.blognhom2.model
+
+
+data class MyPost (
+    val time : String,
+    val img : String,
+    val title : String,
+    val category: String,
+    val content: String
+)
