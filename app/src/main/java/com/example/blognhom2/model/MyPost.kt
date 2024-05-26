509e6ffbbbefff3c4f34a0cb06484aae5df706a1
Nguyễn Trọng Phúc <phucxauxi929@gmail.com>
2024-05-26 18:33:41 +0700

    bookmarkapi

--- app/src/main/java/com/example/blognhom2/model/MyPost.kt
+++ app/src/main/java/com/example/blognhom2/model/MyPost.kt
@@ -2,8 +2,8 @@ package com.example.blognhom2.model
 
 
 data class MyPost (
-    val time : String,
-    val img : String,
+    val id: Int?,
+    val img : String?,
     val title : String,
     val category: String,
     val content: String
