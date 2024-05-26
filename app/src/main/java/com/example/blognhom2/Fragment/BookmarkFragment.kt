a35ba7fefa1e52f2d727a55c4401074e19b876d0
Trịnh Quý <usoppgod2003@gmail.com>
2024-05-26 07:53:40 +0700

    bookmark

--- app/src/main/java/com/example/blognhom2/Fragment/BookmarkFragment.kt
+++ app/src/main/java/com/example/blognhom2/Fragment/BookmarkFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.blognhom2.Adapter.PostAdapter
 import com.example.blognhom2.R
+import com.example.blognhom2.databinding.FragmentBookmarkBinding
+import com.example.blognhom2.databinding.FragmentHomeBinding
+import com.example.blognhom2.model.PostInfo
 
 
 class BookmarkFragment : Fragment() {
-
+    private var _binding: FragmentBookmarkBinding? = null
+    lateinit var bookmarkPost: MutableList<PostInfo>
+    lateinit var adapter : PostAdapter
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+        _binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+
+        val view = binding.root
+        return view
     }
+
+
 }
--- app/src/main/java/com/example/blognhom2/Fragment/PostContentFragment.kt
+++ app/src/main/java/com/example/blognhom2/Fragment/PostContentFragment.kt
@@ -6,14 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.example.blognhom2.API.BlogOwnerApi
+import com.example.blognhom2.API.PostApi
 import com.example.blognhom2.databinding.FragmentPostContentBinding
+import com.example.blognhom2.model.Category
 import com.example.blognhom2.model.PostInfo
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class PostContentFragment : Fragment() {
 
-    lateinit var post : PostInfo;
-    lateinit var bookmarkPost : List<PostInfo>
+    lateinit var post: PostInfo;
+    lateinit var bookmarkPost: MutableList<PostInfo>
     private var _binding: FragmentPostContentBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -23,6 +31,7 @@ class PostContentFragment : Fragment() {
         _binding = FragmentPostContentBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         SetDataForPostContent()
+        bookmarkPost = getPostsInBookmarks() as MutableList<PostInfo>
         val view = binding.root
         return view
     }
@@ -31,9 +40,9 @@ class PostContentFragment : Fragment() {
         this.post = post
     }
 
-    fun SetDataForPostContent(){
-        binding.postTitle.text = "      "+ post.title
-        binding.postContent.text =  "      "+ post.content
+    public fun SetDataForPostContent() {
+        binding.postTitle.text = "      " + post.title
+        binding.postContent.text = "      " + post.content
         binding.postUser.text = post.user
         binding.postCategories.text = post.category
         binding.postTime.text = post.time
@@ -41,11 +50,42 @@ class PostContentFragment : Fragment() {
             .load(post.img)
             .into(binding.postImage)
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-    fun addPostToBookmarks(){
 
+
+    private fun getPostsInBookmarks() : List<PostInfo> {
+        bookmarkPost.clear()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:8081/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val api = retrofit.create(PostApi::class.java)
+        val call = api.getPosts(0);
+
+        call.enqueue(object : Callback<List<PostInfo>> {
+            override fun onResponse(call: Call<List<PostInfo>>, response: Response<List<PostInfo>>) {
+                println("ResponsePost")
+                println(response)
+                if (!response.isSuccessful) {
+                    println("Code: " + response.code())
+                    return
+                }
+
+                val posts = response.body()
+                posts?.let {
+                    bookmarkPost.addAll(it);
+                }
+
+            }
+            override fun onFailure(call: Call<List<PostInfo>>, t: Throwable) {
+                println(t.message)
+            }
+        })
+
+        return bookmarkPost
     }
 }
